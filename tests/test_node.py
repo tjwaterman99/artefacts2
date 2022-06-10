959e89c5643f738228e69312e0ce06373ac5409c
Tom Waterman <tjwaterman99@gmail.com>
2022-06-10 10:16:50 +0200

    Add more tests for Node objects (#10)

---
+++ tests/test_node.py
@@ -0,0 +1,27 @@
+from artefacts.core import Node
+
+
+def test_node_parents(manifest):
+    for model in manifest.models.values():
+        assert model.parents is not None
+        for node in model.parents:
+            assert node is not None, f"{model} has an undefined parent"
+            assert str(node) == node.unique_id
+
+
+def test_node_children(manifest):
+    for model in manifest.models.values():
+        assert model.children is not None
+        for node in model.children:
+            assert node is not None, f"{model} has an undefined child"
+            assert str(node) == node.unique_id
+
+
+def test_node_str(manifest):
+    for model in manifest.models.values():
+        assert str(model) == model.unique_id
+
+
+def test_node_repr(manifest):
+    for model in manifest.models.values():
+        assert repr(model) is not None
