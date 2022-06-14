5d56063a240b0237c55a1b14a58cd40cd2cbaec6
Tom Waterman <tjwaterman99@gmail.com>
2022-06-14 22:13:42 +0200

    Add node methods for Model and Test (#12)

--- artefacts/core.py
+++ artefacts/core.py
@@ -53,11 +53,296 @@ class Node:
 
 
 class Model(Node):
-    pass
+    @property
+    def alias(self):
+        return self._node.alias
+
+    @property
+    def checksum(self):
+        return self._node.checksum
+
+    @property
+    def columns(self):
+        return self._node.columns
+
+    @property
+    def compiled_path(self):
+        return self._node.compiled_path
+
+    @property
+    def config(self):
+        return self._node.config
+
+    @property
+    def created_at(self):
+        return self._node.created_at
+
+    @property
+    def database(self):
+        return self._node.database
+
+    @property
+    def deferred(self):
+        return self._node.deferred
+
+    @property
+    def description(self):
+        return self._node.description
+
+    @property
+    def docs(self):
+        return self._node.docs
+
+    @property
+    def empty(self):
+        return self._node.empty
+
+    @property
+    def file_id(self):
+        return self._node.file_id
+
+    @property
+    def fqn(self):
+        return self._node.fqn
+
+    @property
+    def materialization(self):
+        return self._node.get_materialization()
+
+    @property
+    def identifier(self):
+        return self._node.identifier
+
+    @property
+    def is_refable(self):
+        return self._node.is_refable
+
+    @property
+    def is_relational(self):
+        return self._node.is_relational
+
+    @property
+    def json_schema(self):
+        return self._node.json_schema
+
+    @property
+    def meta(self):
+        return self._node.meta
+
+    @property
+    def name(self):
+
+        return self._node.name
+
+    @property
+    def node_info(self):
+        return self._node.node_info
+
+    @property
+    def original_file_path(self):
+        return self._node.original_file_path
+
+    @property
+    def package_name(self):
+        return self._node.package_name
+
+    @property
+    def path(self):
+        return self._node.path
+
+    @property
+    def raw_sql(self):
+        return self._node.raw_sql
+
+    @property
+    def refs(self):
+        return self._node.refs
+
+    @property
+    def root_path(self):
+        return self._node.root_path
+
+    @property
+    def schema(self):
+        return self._node.schema
+
+    @property
+    def should_store_failures(self):
+        return self._node.should_store_failures
+
+    @property
+    def sources(self):
+        return self._node.sources
+
+    @property
+    def tags(self):
+        return self._node.tags
+
+    @property
+    def to_dict(self):
+        return self._node.to_dict
+
+    @property
+    def unrendered_config(self):
+        return self._node.unrendered_config
 
 
 class Test(Node):
-    pass
+    @property
+    def alias(self):
+        return self._node.alias
+
+    @property
+    def build_path(self):
+        return self._node.build_path
+
+    @property
+    def checksum(self):
+        return self._node.checksum
+
+    @property
+    def column_name(self):
+        return self._node.column_name
+
+    @property
+    def columns(self):
+        return self._node.columns
+
+    @property
+    def compiled_path(self):
+        return self._node.compiled_path
+
+    @property
+    def config(self):
+        return self._node.config
+
+    @property
+    def created_at(self):
+        return self._node.created_at
+
+    @property
+    def database(self):
+        return self._node.database
+
+    @property
+    def deferred(self):
+        return self._node.deferred
+
+    @property
+    def description(self):
+        return self._node.description
+
+    @property
+    def docs(self):
+        return self._node.docs
+
+    @property
+    def empty(self):
+        return self._node.empty
+
+    @property
+    def field_mapping(self):
+        return self._node.field_mapping
+
+    @property
+    def file_id(self):
+        return self._node.file_id
+
+    @property
+    def file_key_name(self):
+        return self._node.file_key_name
+
+    @property
+    def fqn(self):
+        return self._node.fqn
+
+    @property
+    def get_materialization(self):
+        return self._node.get_materialization
+
+    @property
+    def identifier(self):
+        return self._node.identifier
+
+    @property
+    def json_schema(self):
+        return self._node.json_schema
+
+    @property
+    def meta(self):
+        return self._node.meta
+
+    @property
+    def name(self):
+        return self._node.name
+
+    @property
+    def node_info(self):
+        return self._node.node_info
+
+    @property
+    def original_file_path(self):
+        return self._node.original_file_path
+
+    @property
+    def package_name(self):
+        return self._node.package_name
+
+    @property
+    def path(self):
+        return self._node.path
+
+    @property
+    def raw_sql(self):
+        return self._node.raw_sql
+
+    @property
+    def refs(self):
+        return self._node.refs
+
+    @property
+    def resource_type(self):
+        return self._node.resource_type
+
+    @property
+    def root_path(self):
+        return self._node.root_path
+
+    @property
+    def schema(self):
+        return self._node.schema
+
+    @property
+    def should_store_failures(self):
+        return self._node.should_store_failures
+
+    @property
+    def sources(self):
+        return self._node.sources
+
+    @property
+    def tags(self):
+        return self._node.tags
+
+    @property
+    def test_metadata(self):
+        return self._node.test_metadata
+
+    @property
+    def test_node_type(self):
+        return self._node.test_node_type
+
+    @property
+    def to_dict(self):
+        return self._node.to_dict
+
+    @property
+    def unique_id(self):
+        return self._node.unique_id
+
+    @property
+    def unrendered_config(self):
+        return self._node.unrendered_config
 
 
 class Source(Node):
